52d050217f15d5de3b2ea4662607c1d0ef3b56c7
Aaron Salley <aaron@aaronsalleydesign.com>
2018-11-26 18:01:58 -0500

    Starting user creation and profile viewing.

---
+++ src/react/routes/controllers/usersController.tsx
@@ -0,0 +1,19 @@
+'use strict';
+
+import logger from 'logger';
+
+class Users {
+  public createUser = (req, res) => {
+    try {
+    } catch (error) {
+    }
+  }
+
+  public listUsers = (req, res) => {
+    try {
+    } catch (error) {
+    }
+  }
+}
+
+export default new Users();

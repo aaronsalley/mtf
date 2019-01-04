1c260f1161ae3eeb21f69fed6ac29d2b24d31773
Aaron Salley <aaron@aaronsalley.com>
2019-01-04 15:54:27 -0500

    Fixed images on events page

--- src/tribe-events/list/title-bar.php
+++ src/tribe-events/list/title-bar.php
@@ -17,12 +17,25 @@
 		<?php do_action( 'tribe_events_before_the_title' ); ?>
 		<h1 class="title"><?php echo tribe_get_events_title() ?></h1>
 		<?php do_action( 'tribe_events_after_the_title' ); ?>
-    <a class="button" href="https://ticketcentral.com/online/default.asp?doWork::WScontent::loadArticle=Load&BOparam::WScontent::loadArticle::article_id=A871863B-5CF9-463D-A054-A89187A9B231" target="_blank"><?php esc_html_e('Support us'); ?></a>
+    <a class="button" href="/donate" target="_blank"><?php esc_html_e('Support us'); ?></a>
   </div>
   <?php for($i = 0; $i < 10; $i++){
-    $instagram[] = '<div class="post">
-      <img class="image" src="#" />
-    </div>';
-  }
-  echo $instagram = '<div id="instagram" class="feed"><div class="wrap">' . implode($instagram) . '</div></div>'; ?>
+		$instagram[] = '<div class="post">
+			<img class="image" src="#" />
+		</div>';
+	}
+
+  $frontpage_id = get_option( 'page_on_front' );
+
+  if ( get_post_gallery($frontpage_id) ) :
+      $gallery = get_post_gallery( $frontpage_id, false );
+      $instagram = array();
+      /* Loop through all the image and output them one by one */
+      foreach( $gallery['src'] as $src ) :
+        $instagram[] = '<div class="post">
+          <img src="' . $src . '" class="image" alt="Gallery image" />
+        </div>';
+      endforeach;
+  endif;
+	echo $instagram = '<div id="instagram" class="feed"><div class="wrap">' . implode($instagram) . '</div></div>'; ?>
 </header>

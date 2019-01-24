9efc088eef9fc72d9298050537fee4db387a9321
Aaron Salley <aaron@aaronsalley.com>
2019-01-23 19:32:37 -0500

    More size changes

--- src/template-parts/blog-posts.php
+++ src/template-parts/blog-posts.php
@@ -9,14 +9,16 @@
     $class = is_sticky() ? 'sticky' : '';
     ?>
     <article <?php post_class($class); ?>>
-      <figure class="image"></figure>
-      <header class="header">
-        <h3 class="lead"><?php the_title(); ?></h3>
-        <p class="meta"></p>
-        <section class="teaser">
-          <?php the_excerpt(); ?>
-        </section>
-      </header>
+      <div class="container">
+        <figure class="image"></figure>
+        <header class="header">
+          <h3 class="lead"><?php the_title(); ?></h3>
+          <p class="meta"></p>
+          <section class="teaser">
+            <?php the_excerpt(); ?>
+          </section>
+        </header>
+      </div>
     </article>
   <?php endwhile; else : ?>
     <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>

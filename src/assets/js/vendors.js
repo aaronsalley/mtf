02d7b8bda2b5a978d9a8ed35a333d16254d298ac
Aaron Salley <aaron@aaronsalley.com>
2019-02-14 14:52:06 -0500

    test for blog before doing JS

--- src/assets/js/vendors.js
+++ src/assets/js/vendors.js
@@ -15,39 +15,44 @@ const navDrawer = new Foundation.OffCanvas($('#nav-drawer'), {
   nested: false,
 });
 
-const msnry = new Masonry( '.blog-posts', {
-  columnWidth: '.grid-sizer',
-  itemSelector: '.post',
-  percentPosition: true,
-});
-
 (function blogFader() {
   const fac = new FastAverageColor();
   const blogPosts = document.querySelector('.blog-posts');
-  const articleImages = blogPosts.querySelectorAll('img');
-
-  let stylesheet = document.createElement('style');
-      stylesheet.appendChild(document.createTextNode(""));
-      document.head.appendChild(stylesheet);
-      stylesheet = stylesheet.sheet;
-
-  let i = 0;
-
-  articleImages.forEach(function(image) {
-    i++;
-
-    fac.getColorAsync(image, function(color) {
-      const regExp = /post-([0-9]+)/;
-      const postID = regExp.exec(this.closest('.post').className);
-      const target = '.' + postID[0] + ' .image::before';
-      const from = 'rgba(' + color.value[0] + ',' + color.value[1] + ',' + color.value[2] + ',0)';
-      const to = 'rgba(' + color.value[0] + ',' + color.value[1] + ',' + color.value[2] + ',1)';
-
-      let style = 'background-image: -webkit-gradient(linear, left top, left bottom, from(' + from + '), to(' + to + ')) !important; background-image: linear-gradient(top, ' + from + ' 0%, ' + to + ' 100%) !important;';
-
-      return stylesheet.addRule(target, style);
+  if(typeof(blogPosts) != 'undefined' && blogPosts != null) {
+    const articleImages = blogPosts.querySelectorAll('img');
+    const msnry = new Masonry( blogPosts, {
+      columnWidth: '.grid-sizer',
+      itemSelector: '.post',
+      percentPosition: true,
     });
-  });
+
+    let stylesheet = document.createElement('style');
+        stylesheet.appendChild(document.createTextNode(""));
+        document.head.appendChild(stylesheet);
+        stylesheet = stylesheet.sheet;
+
+    let i = 0;
+
+    articleImages.forEach(function(image) {
+      i++;
+
+      fac.getColorAsync(image, function(color) {
+        const regExp = /post-([0-9]+)/;
+        const postID = regExp.exec(this.closest('.post').className);
+        const target = '.' + postID[0] + ' .image::before';
+        const from = 'rgba(' + color.value[0] + ',' + color.value[1] + ',' + color.value[2] + ',0)';
+        const to = 'rgba(' + color.value[0] + ',' + color.value[1] + ',' + color.value[2] + ',1)';
+
+        let style = 'background-image: -webkit-gradient(linear, left top, left bottom, from(' + from + '), to(' + to + ')) !important; background-image: linear-gradient(top, ' + from + ' 0%, ' + to + ' 100%) !important;';
+
+        return stylesheet.addRule(target, style);
+      });
+    });
+  } else {
+
+    return false;
+
+  }
 })();
 
 if (process.env.NODE_ENV == 'production') {
--- src/template-parts/header-masthead.php
+++ src/template-parts/header-masthead.php
@@ -1,6 +1,15 @@
 <header class="header">
 	<div class="masthead">
-		<h2 class="title"><?php the_title(); ?></h2>
+		<h2 class="title"><?php
+		$categories = get_the_category();
+		$title = get_the_title();
+		
+		if ( ! empty( $categories ) ) {
+		    $title = esc_html( $categories[0]->name );
+		}
+
+		echo $title;
+		?></h2>
 		<a class="button" href="/donate" target="_blank"><?php esc_html_e('Support us'); ?></a>
 	</div>
 	<?php get_template_part('template-parts/gallery'); ?>

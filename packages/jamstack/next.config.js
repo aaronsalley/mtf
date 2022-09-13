5990cb31ec92face8e89b23be195bbd36cef98ce
Aaron Salley <aaron@aaronsalley.com>
2022-09-12 23:37:11 -0400

    Renabled Sentry

--- packages/jamstack/next.config.js
+++ packages/jamstack/next.config.js
@@ -4,7 +4,7 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 /** @type {import('next').NextConfig} */
-// const { withSentryConfig } = require('@sentry/nextjs');
+const { withSentryConfig } = require('@sentry/nextjs');
 
 const nextConfig = {
   poweredByHeader: false,
@@ -40,5 +40,5 @@ const sentryWebpackPluginOptions = {
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = moduleExports;
-// module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+// module.exports = moduleExports;
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);

2ba294bf929c2225fadd74cee5e93a97ab820f92
Aaron Salley <aaron@aaronsalley.com>
2022-12-06 22:32:23 -0500

    Remove dates, add cats for homepage news

--- packages/wordpress/theme/components/article-tile.php
+++ packages/wordpress/theme/components/article-tile.php
@@ -1,10 +1,30 @@
+<?php
+$__terms = [];
+
+if ($taxonomy === 'tags') {
+  $__terms = get_the_tags();
+} else {
+  $__terms = get_the_category();
+}
+
+if ($__terms) {
+  $terms = [];
+
+  foreach ($__terms as $term) {
+    $terms[] = $term->name;
+  }
+
+  $terms = implode(' ', $terms);
+}
+
+?>
 <article class="Article_tile_container">
   <a href=<?php the_permalink(); ?>>
+    <span><?php echo $terms; ?></span>
     <header>
       <h3><?php the_title(); ?></h3>
     </header>
     <section>
-      <time><?php the_date(); ?></time>
       <div><?php the_excerpt(); ?></div>
     </section>
     <footer>
